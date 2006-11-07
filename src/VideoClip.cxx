b221a72c03c2a8973ff11503fa3a1f2c4dba9570
richard.spindler <richard.spindler@gmail.com>
2006-11-07 09:31:59 +0100

    cassert
    darcs-hash:20061107083159-7aa3a-907c3e481a160925cee82be23114021195aed7bb

--- src/VideoClip.cxx
+++ src/VideoClip.cxx
@@ -17,6 +17,8 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include <cassert>
+
 #include "VideoClip.H"
 #include "IVideoFile.H"
 #include "FilmStrip.H"
