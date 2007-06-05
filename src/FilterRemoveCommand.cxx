b4dc30d7217add386766a2e29da89b0c71705260
richard.spindler <richard.spindler@gmail.com>
2007-06-05 15:19:32 +0200

    cassert fixed
    darcs-hash:20070605131932-7aa3a-4953e8a2db47807fee88bee05a809af70139a424

--- src/FilterRemoveCommand.cxx
+++ src/FilterRemoveCommand.cxx
@@ -17,6 +17,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include <cassert>
 #include "FilterRemoveCommand.H"
 #include "FilterClip.H"
 #include "timeline/Track.H"
