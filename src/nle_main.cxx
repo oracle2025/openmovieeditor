a26349dcb3d291016d75a95dcb3ae105daf80bd7
richard.spindler <richard.spindler@gmail.com>
2007-10-08 18:58:22 +0200

    disabled inkscape title
    darcs-hash:20071008165822-7aa3a-aea57ece915575db24ea388a97b38dbb896c54d0

--- src/nle_main.cxx
+++ src/nle_main.cxx
@@ -139,7 +139,7 @@ int main( int argc, char** argv )
 	NleUI nui;
 	nle::g_ui = &nui;
 	nui.special_clips->add("Titles", nle::PL_VIDEO_SRC, "src:builtin:TitleClip" );
-	nui.special_clips->add("Inkscape Title", nle::PL_VIDEO_SRC, "src:builtin:InkscapeClip" );
+/*	nui.special_clips->add("Inkscape Title", nle::PL_VIDEO_SRC, "src:builtin:InkscapeClip" );*/
 	nui.special_clips->add("Volume Automations", nle::PL_AUDIO_FILTER, "filter:builtin:VolumeAutomations" );
 	nui.special_clips->add("Color Curves", nle::PL_VIDEO_EFFECT, "effect:builtin:ColorCurves" );
 
